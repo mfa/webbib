39d386afd0bf62bac6f7e600d74234c7a733f78e
Andreas Madsack <andreas.madsack@ims.uni-stuttgart.de>
2012-12-01 20:14:52 +0100

    create external example

---
+++ example/config.py
@@ -0,0 +1,8 @@
+#
+#
+#
+
+# generate a new one: import os; os.urandom(24)
+SECRET_KEY = 'fhttzWoxxv0mgxxwzoJpaqvfqocbpqkkfzc]izeog6ovivvdtr'
+BIB_FILENAME = 'example.bib'
+STAFF_FILENAME = 'staff.xml'
